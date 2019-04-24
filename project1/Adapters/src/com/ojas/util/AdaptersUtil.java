1e39fca38c5480a1adc38c65cf4749cfdb68885e
RachanaReddigari <49934885+RachanaReddigari@users.noreply.github.com>
2019-04-24 16:33:38 +0530

    modified AdaptersUtil.java

--- project1/Adapters/src/com/ojas/util/AdaptersUtil.java
+++ project1/Adapters/src/com/ojas/util/AdaptersUtil.java
@@ -1,12 +1,24 @@
 package com.ojas.util;
 
+import oracle.core.ojdl.logging.ODLLevel;
 import oracle.core.ojdl.logging.ODLLogger;
+import oracle.iam.identity.usermgmt.api.UserManager;
+import oracle.iam.platform.Platform;
 
 public class AdaptersUtil {
 	
 	public static final ODLLogger LOGGER = ODLLogger.getODLLogger(AdapterConstants.ADAPTER_LOG);
+	public static final String CLASSNAME = AdaptersUtil.class.getName();
 	
-	public void getUserLogin(int userKey) {
+	public String getUserLogin(String userKey) {
+		String userLogin = null;
+		UserManager umgr = Platform.getService(UserManager.class);
+		try {
+			userLogin = umgr.getDetails(userKey, null, false).getLogin();
+		} catch (Exception e){
+			LOGGER.log(ODLLevel.ERROR, CLASSNAME + " getUserLogin() : " + e);
+		}
+		return userLogin;
 		
 	}
 
