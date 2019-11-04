113dc48bf2108558b11c797907b5e3e835da48e6
kamalakshibale <49933522+kamalakshibale@users.noreply.github.com>
2019-11-04 14:41:10 +0530

    added new Variable in SchedulerConstants.java file

--- project1/Schedulers/src/com/ojas/util/SchedulerConstants.java
+++ project1/Schedulers/src/com/ojas/util/SchedulerConstants.java
@@ -2,5 +2,6 @@ package com.ojas.util;
 
 public class SchedulerConstants {
 	public static final String SCHEDULER_LOG = "Scheduler_custom_log";
+	public static final String V1 = "Custom Variable";
 
 }
